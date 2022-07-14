9ad72693e6b71d72943add752863248884562508
Drummer Dee <drummer.deja@gmail.com>
2022-07-14 17:54:20 -0400

    nav bar

---
+++ .history/public/js/mindfulness_20220713144429.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
